db4db486cdcc1b3a8bf6d948dab1985cbb35cb0a
M1cha <sigmaepsilon92@gmail.com>
2017-03-24 18:03:26 +0100

    cleanup display related stuff and add 2ndstage display and display console support

---
+++ Qcom/Library/DisplaySerialPortLib/DisplaySerialPortLib.c
@@ -0,0 +1,183 @@
+#include <PiDxe.h>
+
+#include <Library/LKEnvLib.h>
+#include <Library/SerialPortLib.h>
+#include <Library/HobLib.h>
+#include <Library/fbcon.h>
+#include <Library/QcomTargetDisplayLib.h>
+
+RETURN_STATUS
+EFIAPI
+SerialPortInitialize (
+  VOID
+  )
+{
+  return RETURN_SUCCESS;
+}
+
+RETURN_STATUS
+EFIAPI
+DisplaySerialPortLibInitialize (
+  VOID
+  )
+{
+  VOID                *Hob;
+  struct fbcon_pos    *cur_pos;
+
+  target_display_init();
+
+  // try to get position hob
+  Hob = GetFirstGuidHob (&gQcomFbConStateGuid);
+  if (Hob != NULL && GET_GUID_HOB_DATA_SIZE (Hob) == sizeof *cur_pos) {
+    cur_pos = GET_GUID_HOB_DATA (Hob);
+    fbcon_set_position(cur_pos);
+  }
+
+  return RETURN_SUCCESS;
+}
+
+/**
+  Write data to serial device.
+
+  @param  Buffer           Point of data buffer which need to be written.
+  @param  NumberOfBytes    Number of output bytes which are cached in Buffer.
+
+  @retval 0                Write data failed.
+  @retval !0               Actual number of bytes written to serial device.
+
+**/
+UINTN
+EFIAPI
+SerialPortWrite (
+  IN UINT8     *Buffer,
+  IN UINTN     NumberOfBytes
+  )
+{
+  UINT8* CONST Final = &Buffer[NumberOfBytes];
+  while (Buffer < Final) {
+    fbcon_putc(*Buffer++);
+  }
+  return NumberOfBytes;
+}
+
+/**
+  Read data from serial device and save the data in buffer.
+
+  @param  Buffer           Point of data buffer which need to be written.
+  @param  NumberOfBytes    Size of Buffer[].
+
+  @retval 0                Read data failed.
+  @retval !0               Actual number of bytes read from serial device.
+
+**/
+UINTN
+EFIAPI
+SerialPortRead (
+  OUT UINT8     *Buffer,
+  IN  UINTN     NumberOfBytes
+)
+{
+  return 0;
+}
+
+/**
+  Check to see if any data is available to be read from the debug device.
+
+  @retval TRUE       At least one byte of data is available to be read
+  @retval FALSE      No data is available to be read
+
+**/
+BOOLEAN
+EFIAPI
+SerialPortPoll (
+  VOID
+  )
+{
+  return FALSE;
+}
+
+/**
+  Sets the control bits on a serial device.
+
+  @param[in] Control            Sets the bits of Control that are settable.
+
+  @retval RETURN_SUCCESS        The new control bits were set on the serial device.
+  @retval RETURN_UNSUPPORTED    The serial device does not support this operation.
+  @retval RETURN_DEVICE_ERROR   The serial device is not functioning correctly.
+
+**/
+RETURN_STATUS
+EFIAPI
+SerialPortSetControl (
+  IN UINT32 Control
+  )
+{
+  return RETURN_UNSUPPORTED;
+}
+
+/**
+  Retrieve the status of the control bits on a serial device.
+
+  @param[out] Control           A pointer to return the current control signals from the serial device.
+
+  @retval RETURN_SUCCESS        The control bits were read from the serial device.
+  @retval RETURN_UNSUPPORTED    The serial device does not support this operation.
+  @retval RETURN_DEVICE_ERROR   The serial device is not functioning correctly.
+
+**/
+RETURN_STATUS
+EFIAPI
+SerialPortGetControl (
+  OUT UINT32 *Control
+  )
+{
+  return RETURN_UNSUPPORTED;
+}
+
+/**
+  Sets the baud rate, receive FIFO depth, transmit/receice time out, parity,
+  data bits, and stop bits on a serial device.
+
+  @param BaudRate           The requested baud rate. A BaudRate value of 0 will use the
+                            device's default interface speed.
+                            On output, the value actually set.
+  @param ReveiveFifoDepth   The requested depth of the FIFO on the receive side of the
+                            serial interface. A ReceiveFifoDepth value of 0 will use
+                            the device's default FIFO depth.
+                            On output, the value actually set.
+  @param Timeout            The requested time out for a single character in microseconds.
+                            This timeout applies to both the transmit and receive side of the
+                            interface. A Timeout value of 0 will use the device's default time
+                            out value.
+                            On output, the value actually set.
+  @param Parity             The type of parity to use on this serial device. A Parity value of
+                            DefaultParity will use the device's default parity value.
+                            On output, the value actually set.
+  @param DataBits           The number of data bits to use on the serial device. A DataBits
+                            vaule of 0 will use the device's default data bit setting.
+                            On output, the value actually set.
+  @param StopBits           The number of stop bits to use on this serial device. A StopBits
+                            value of DefaultStopBits will use the device's default number of
+                            stop bits.
+                            On output, the value actually set.
+
+  @retval RETURN_SUCCESS            The new attributes were set on the serial device.
+  @retval RETURN_UNSUPPORTED        The serial device does not support this operation.
+  @retval RETURN_INVALID_PARAMETER  One or more of the attributes has an unsupported value.
+  @retval RETURN_DEVICE_ERROR       The serial device is not functioning correctly.
+
+**/
+RETURN_STATUS
+EFIAPI
+SerialPortSetAttributes (
+  IN OUT UINT64             *BaudRate,
+  IN OUT UINT32             *ReceiveFifoDepth,
+  IN OUT UINT32             *Timeout,
+  IN OUT EFI_PARITY_TYPE    *Parity,
+  IN OUT UINT8              *DataBits,
+  IN OUT EFI_STOP_BITS_TYPE *StopBits
+  )
+{
+  return RETURN_UNSUPPORTED;
+}
+
---
+++ Qcom/Platforms/Msm8960/Library/QcomTarget2ndStageDisplayLib/target_display.c
@@ -0,0 +1,37 @@
+#include <Library/LKEnvLib.h>
+#include <Library/fbcon.h>
+#include <Chipset/mdp4.h>
+#include <Chipset/lcdc.h>
+#include <Chipset/mipi_dsi.h>
+#include <Platform/display.h>
+
+static struct fbcon_config config = {0};
+
+void target_display_init(void)
+{
+    // dump format
+    config.base = (void *) readl(MDP_DMA_P_BUF_ADDR);
+    config.width = readl(MDP_DMA_P_BUF_Y_STRIDE)/3;
+    config.height = readl(MDP_DMA_P_SIZE)>>16;
+    config.stride = config.width;
+    config.bpp = 24;
+    config.format = FB_FORMAT_RGB888;
+
+    // set rgp swap
+#ifdef DISPLAY_RGBSWAP
+    unsigned char DST_FORMAT = 8;
+    int data = 0x00100000;
+    data |= ((DISPLAY_RGBSWAP & 0x07) << 16);
+    writel(data | DST_FORMAT, DSI_COMMAND_MODE_MDP_CTRL);
+#endif
+
+    // handle cmd trigger
+    uint32_t trigger_ctrl = readl(DSI_TRIG_CTRL);
+    int mdp_trigger = (trigger_ctrl >> 4) & 4;
+    if (mdp_trigger == DSI_CMD_TRIGGER_SW) {
+        config.update_start = mipi_update_sw_trigger;
+    }
+
+    // setup fbcon
+    fbcon_setup(&config);
+}
