45713cbbdf0f11544294433aa5db41ed71affb3a
M1cha <sigmaepsilon92@gmail.com>
2017-03-25 16:39:37 +0100

    Library/EFIDroidRelocatablePlatformLib: Remap 'remove-completely' regions as device memory
    this prevents the hypervisor from rebooting the device

--- Library/EFIDroidRelocatablePlatformLib/EFIDroidMem.c
+++ Library/EFIDroidRelocatablePlatformLib/EFIDroidMem.c
@@ -19,8 +19,10 @@
 #include <Library/IoLib.h>
 #include <Library/MemoryAllocationLib.h>
 
+#include <libfdt.h>
+
 // Number of Virtual Memory Map Descriptors
-#define MAX_VIRTUAL_MEMORY_MAP_DESCRIPTORS          5
+#define MAX_VIRTUAL_MEMORY_MAP_DESCRIPTORS          20
 
 // DDR attributes
 #define DDR_ATTRIBUTES_CACHED    ARM_MEMORY_REGION_ATTRIBUTE_WRITE_BACK
@@ -49,6 +51,9 @@ ArmPlatformGetVirtualMemoryMap (
   )
 {
   ARM_MEMORY_REGION_DESCRIPTOR  *VirtualMemoryTable;
+  VOID                          *Fdt;
+  INTN                          Index = 0;
+  INTN                          IndexDram = -1;
 
   ASSERT (VirtualMemoryMap != NULL);
 
@@ -65,42 +70,105 @@ ArmPlatformGetVirtualMemoryMap (
   }
 
   // System DRAM
-  VirtualMemoryTable[0].PhysicalBase = PcdGet64 (PcdSystemMemoryBase);
-  VirtualMemoryTable[0].VirtualBase  = VirtualMemoryTable[0].PhysicalBase;
-  VirtualMemoryTable[0].Length       = PcdGet64 (PcdSystemMemorySize);
-  VirtualMemoryTable[0].Attributes   = DDR_ATTRIBUTES_CACHED;
+  VirtualMemoryTable[Index].PhysicalBase = PcdGet64 (PcdSystemMemoryBase);
+  VirtualMemoryTable[Index].VirtualBase  = VirtualMemoryTable[Index].PhysicalBase;
+  VirtualMemoryTable[Index].Length       = PcdGet64 (PcdSystemMemorySize);
+  VirtualMemoryTable[Index].Attributes   = DDR_ATTRIBUTES_CACHED;
+  IndexDram = Index++;
 
   DEBUG ((EFI_D_INFO, "%a: Dumping System DRAM Memory Map:\n"
       "\tPhysicalBase: 0x%lX\n"
       "\tVirtualBase: 0x%lX\n"
       "\tLength: 0x%lX\n",
       __FUNCTION__,
-      VirtualMemoryTable[0].PhysicalBase,
-      VirtualMemoryTable[0].VirtualBase,
-      VirtualMemoryTable[0].Length));
+      VirtualMemoryTable[IndexDram].PhysicalBase,
+      VirtualMemoryTable[IndexDram].VirtualBase,
+      VirtualMemoryTable[IndexDram].Length));
 
   // Peripheral space before DRAM
-  VirtualMemoryTable[1].PhysicalBase = 0x0;
-  VirtualMemoryTable[1].VirtualBase  = 0x0;
-  VirtualMemoryTable[1].Length       = VirtualMemoryTable[0].PhysicalBase;
-  VirtualMemoryTable[1].Attributes   = ARM_MEMORY_REGION_ATTRIBUTE_DEVICE;
+  if (VirtualMemoryTable[IndexDram].PhysicalBase != 0) {
+    VirtualMemoryTable[Index].PhysicalBase = 0x0;
+    VirtualMemoryTable[Index].VirtualBase  = 0x0;
+    VirtualMemoryTable[Index].Length       = VirtualMemoryTable[IndexDram].PhysicalBase;
+    VirtualMemoryTable[Index++].Attributes   = ARM_MEMORY_REGION_ATTRIBUTE_DEVICE;
+  }
 
   // Peripheral space after DRAM
-  VirtualMemoryTable[2].PhysicalBase = VirtualMemoryTable[0].Length + VirtualMemoryTable[1].Length;
-  VirtualMemoryTable[2].VirtualBase  = VirtualMemoryTable[2].PhysicalBase;
-  VirtualMemoryTable[2].Length       = MIN (1ULL << FixedPcdGet8 (PcdPrePiCpuMemorySize),
+  VirtualMemoryTable[Index].PhysicalBase = VirtualMemoryTable[IndexDram].PhysicalBase + VirtualMemoryTable[IndexDram].Length;
+  VirtualMemoryTable[Index].VirtualBase  = VirtualMemoryTable[Index].PhysicalBase;
+  VirtualMemoryTable[Index].Length       = MIN (1ULL << FixedPcdGet8 (PcdPrePiCpuMemorySize),
                                          ArmGetPhysAddrTop ()) -
-                                       VirtualMemoryTable[2].PhysicalBase;
-  VirtualMemoryTable[2].Attributes   = ARM_MEMORY_REGION_ATTRIBUTE_DEVICE;
+                                       VirtualMemoryTable[Index].PhysicalBase;
+  VirtualMemoryTable[Index++].Attributes   = ARM_MEMORY_REGION_ATTRIBUTE_DEVICE;
 
   // Remap the FD region as normal executable memory
-  VirtualMemoryTable[3].PhysicalBase = PcdGet64 (PcdFdBaseAddress);
-  VirtualMemoryTable[3].VirtualBase  = VirtualMemoryTable[3].PhysicalBase;
-  VirtualMemoryTable[3].Length       = FixedPcdGet32 (PcdFdSize);
-  VirtualMemoryTable[3].Attributes   = DDR_ATTRIBUTES_CACHED;
+  VirtualMemoryTable[Index].PhysicalBase = PcdGet64 (PcdFdBaseAddress);
+  VirtualMemoryTable[Index].VirtualBase  = VirtualMemoryTable[Index].PhysicalBase;
+  VirtualMemoryTable[Index].Length       = FixedPcdGet32 (PcdFdSize);
+  VirtualMemoryTable[Index++].Attributes   = DDR_ATTRIBUTES_CACHED;
 
+  // Remap 'remove-completely' regions as device memory to prevent the hypervisor from rebooting the device
+  Fdt = (VOID*)(UINTN)PcdGet64 (PcdDeviceTreeInitialBaseAddress);
+  if (Fdt != NULL && fdt_check_header (Fdt) == 0) {
+    INT32         MemoryNode;
+    INT32         Offset;
+    INT32         AddressCells = 1;
+    INT32         SizeCells = 1;
+    INT32         Length;
+    CONST INT32   *Prop;
+
+    MemoryNode = fdt_path_offset (Fdt, "/memory");
+    if (MemoryNode <= 0) {
+      goto END_OF_TABLE;
+    }
+
+    Prop = fdt_getprop (Fdt, MemoryNode, "#address-cells", &Length);
+    if (Length == 4) {
+      AddressCells = fdt32_to_cpu (*Prop);
+    }
+
+    Prop = fdt_getprop (Fdt, MemoryNode, "#size-cells", &Length);
+    if (Length == 4) {
+      SizeCells = fdt32_to_cpu (*Prop);
+    }
+
+    for (Offset = fdt_first_subnode(Fdt, MemoryNode); Offset>=0; Offset = fdt_next_subnode(Fdt, Offset)) {
+      UINT64 LocalBase;
+      UINT64 LocalSize;
+
+      Prop = fdt_getprop (Fdt, Offset, "linux,remove-completely", &Length);
+      if (Prop == NULL) {
+        continue;
+      }
+
+      Prop = fdt_getprop (Fdt, Offset, "reg", &Length);
+      if (Length != ((AddressCells + SizeCells) * sizeof (INT32))) {
+        continue;
+      }
+
+      LocalBase = fdt32_to_cpu (Prop[0]);
+      if (AddressCells > 1) {
+        LocalBase = (LocalBase << 32) | fdt32_to_cpu (Prop[1]);
+      }
+      Prop += AddressCells;
+
+      LocalSize = fdt32_to_cpu (Prop[0]);
+      if (SizeCells > 1) {
+        LocalSize = (LocalSize << 32) | fdt32_to_cpu (Prop[1]);
+      }
+      Prop += SizeCells;
+
+      ASSERT(Index<MAX_VIRTUAL_MEMORY_MAP_DESCRIPTORS-1);
+      VirtualMemoryTable[Index].PhysicalBase = LocalBase;
+      VirtualMemoryTable[Index].VirtualBase  = VirtualMemoryTable[Index].PhysicalBase;
+      VirtualMemoryTable[Index].Length       = LocalSize;
+      VirtualMemoryTable[Index++].Attributes   = ARM_MEMORY_REGION_ATTRIBUTE_DEVICE;
+    }
+  }
+
+END_OF_TABLE:
   // End of Table
-  ZeroMem (&VirtualMemoryTable[4], sizeof (ARM_MEMORY_REGION_DESCRIPTOR));
+  ZeroMem (&VirtualMemoryTable[Index++], sizeof (ARM_MEMORY_REGION_DESCRIPTOR));
 
   *VirtualMemoryMap = VirtualMemoryTable;
 }
