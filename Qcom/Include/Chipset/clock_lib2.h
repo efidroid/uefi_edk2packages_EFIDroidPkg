8a89920d66bfafc8843ab38faf57e9b5dcf5f52d
M1cha <sigmaepsilon92@gmail.com>
2017-03-25 17:16:39 +0100

    Qcom/Include/Chipset/clock_lib2.h: add F_MM macro

--- Qcom/Include/Chipset/clock_lib2.h
+++ Qcom/Include/Chipset/clock_lib2.h
@@ -68,6 +68,18 @@
                               | BVAL(10, 8, s##_source_val), \
         }
 
+/* F_MM(frequency, source, div, m, n) */
+#define F_MM(f, s, div, m, n) \
+	{ \
+		.freq_hz = (f), \
+		.src_clk = &s##_clk_src.c, \
+		.m_val = (m), \
+		.n_val = ~((n)-(m)) * !!(n), \
+		.d_val = ~(n),\
+		.div_src_val = BVAL(4, 0, (int)(2*(div) - 1)) \
+			| BVAL(10, 8, s##_mm_source_val), \
+	}
+
 #define F_MDSS(f, s, div, m, n) \
 	{ \
 		.freq_hz = (f), \
