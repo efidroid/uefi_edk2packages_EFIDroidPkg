7997a9c600bcc05da9242761cef95b023c067ff2
M1cha <sigmaepsilon92@gmail.com>
2017-03-27 19:49:02 +0200

    Qcom/Include/Library/QcomPlatformGpioTlmmLib.h: rename include guard

--- Qcom/Include/Library/QcomPlatformGpioTlmmLib.h
+++ Qcom/Include/Library/QcomPlatformGpioTlmmLib.h
@@ -1,5 +1,5 @@
-#ifndef __LIBRARY_QCOM_PLATFORM_TIMER_LIB_H__
-#define __LIBRARY_QCOM_PLATFORM_TIMER_LIB_H__
+#ifndef __LIBRARY_QCOM_PLATFORM_GPIO_TLMM_LIB_H__
+#define __LIBRARY_QCOM_PLATFORM_GPIO_TLMM_LIB_H__
 
 void gpio_tlmm_config(uint32_t gpio, uint8_t func, uint8_t dir, uint8_t pull, uint8_t drvstr, uint32_t enable);
 void gpio_set(uint32_t gpio, uint32_t dir);
