d95f11ad847a9488035c978f18d14afb0dea7e4f
M1cha <sigmaepsilon92@gmail.com>
2017-03-27 11:54:42 +0200

    Include/Library/LKEnvLib.h: add strstr

--- Include/Library/LKEnvLib.h
+++ Include/Library/LKEnvLib.h
@@ -56,6 +56,7 @@ typedef UINTN paddr_t;
 #define strncmp(s1, s2, n) ((int)AsciiStrnCmp((s1), (s2), (n)))
 #define strlen(s) ((size_t)AsciiStrLen((s)))
 #define strlcpy(dst, src, n) AsciiStrCpyS((dst), (n), (src))
+#define strstr(s1 , s2) AsciiStrStr((s1), (s2))
 #define memset(s, c, n) SetMem((s), (UINTN)(n), (UINT8)(c))
 #define memcpy(s1, s2, n) CopyMem((s1), (s2), (n))
 #define memmove(s1, s2, n) CopyMem((s1), (s2), (n))
