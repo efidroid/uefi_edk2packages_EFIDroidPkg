09d899712101226a407d827bb20b6ca8f18a1930
M1cha <sigmaepsilon92@gmail.com>
2017-03-30 13:48:14 +0200

    Library/PlatformBootManagerLib: add gEFIDroidKeypadDeviceProtocolGuid devices to ConIn

--- Library/PlatformBootManagerLib/PlatformBm.c
+++ Library/PlatformBootManagerLib/PlatformBm.c
@@ -326,6 +326,35 @@ AddOutput (
 
 STATIC
 VOID
+EFIAPI
+AddInput (
+  IN EFI_HANDLE   Handle,
+  IN CONST CHAR16 *ReportText
+  )
+{
+  EFI_STATUS               Status;
+  EFI_DEVICE_PATH_PROTOCOL *DevicePath;
+
+  DevicePath = DevicePathFromHandle (Handle);
+  if (DevicePath == NULL) {
+    DEBUG ((EFI_D_ERROR, "%a: %s: handle %p: device path not found\n",
+      __FUNCTION__, ReportText, Handle));
+    return;
+  }
+
+  Status = EfiBootManagerUpdateConsoleVariable (ConIn, DevicePath, NULL);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((EFI_D_ERROR, "%a: %s: adding to ConIn: %r\n", __FUNCTION__,
+      ReportText, Status));
+    return;
+  }
+
+  DEBUG ((EFI_D_VERBOSE, "%a: %s: added to ConOut and ErrOut\n", __FUNCTION__,
+    ReportText));
+}
+
+STATIC
+VOID
 PlatformRegisterFvBootOption (
   EFI_GUID                         *FileGuid,
   CHAR16                           *Description,
@@ -511,6 +540,12 @@ PlatformBootManagerBeforeConsole (
   // ErrOut.
   //
   FilterAndProcess (&gEfiGraphicsOutputProtocolGuid, NULL, AddOutput);
+
+  //
+  // Now add the device path of all handles with QcomKeypadDeviceProtocolGuid
+  // on them to ConIn.
+  //
+  FilterAndProcess (&gEFIDroidKeypadDeviceProtocolGuid, NULL, AddInput);
 
   //
   // Add the hardcoded short-form USB keyboard device path to ConIn.
