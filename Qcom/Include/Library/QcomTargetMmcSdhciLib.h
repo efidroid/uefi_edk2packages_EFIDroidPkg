d02db856087c2e586ea15ba2cefc35361af0c04d
M1cha <sigmaepsilon92@gmail.com>
2017-04-17 13:59:13 +0200

    add SdhciMMCHSDxe

---
+++ Qcom/Include/Library/QcomTargetMmcSdhciLib.h
@@ -0,0 +1,11 @@
+#ifndef __LIBRARY_QCOM_TARGET_MMC_LIB_H__
+#define __LIBRARY_QCOM_TARGET_MMC_LIB_H__
+
+#include <Chipset/mmc_sdhci.h>
+
+typedef struct mmc_device* (*INIT_SLOT_CB)(struct mmc_config_data *config);
+
+VOID LibQcomTargetMmcSdhciInit(INIT_SLOT_CB InitSlot);
+UINT32 LibQcomTargetMmcSdhciDdrCfgVal(VOID);
+
+#endif
