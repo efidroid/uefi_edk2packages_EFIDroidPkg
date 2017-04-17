95790d36a7d543cd93fc896608f53998d2edf23d
M1cha <sigmaepsilon92@gmail.com>
2017-04-17 12:45:15 +0200

    Qcom/Drivers/Pm8x41Dxe/pm8x41.c: stop printing GPIO status

--- Qcom/Drivers/Pm8x41Dxe/pm8x41.c
+++ Qcom/Drivers/Pm8x41Dxe/pm8x41.c
@@ -153,8 +153,6 @@ int pm8x41_gpio_get(uint8_t gpio, uint8_t *status)
 	/* Return the value of the GPIO pin */
 	*status &= BIT(GPIO_STATUS_VAL_BIT);
 
-	dprintf(SPEW, "GPIO %d status is %d\n", gpio, *status);
-
 	return 0;
 }
 
@@ -227,7 +225,6 @@ int pm8x41_gpio_get_sid(uint8_t sid, uint8_t gpio, uint8_t *status)
 	/* Return the value of the GPIO pin */
 	*status &= BIT(GPIO_STATUS_VAL_BIT);
 
-	dprintf(SPEW, "GPIO %d status is %d\n", gpio, *status);
 
 	return 0;
 }
