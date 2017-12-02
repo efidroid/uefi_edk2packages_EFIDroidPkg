cbf1b101d2a3032b1f4953364094b5c67c2342c0
M1cha <sigmaepsilon92@gmail.com>
2017-12-02 22:37:23 +0100

    Library/MallocLib: implement without lists

--- Library/MallocLib/malloc.c
+++ Library/MallocLib/malloc.c
@@ -2,74 +2,169 @@
 #include <Uefi.h>
 #include <Library/BaseLib.h>
 #include <Library/DebugLib.h>
+#include <Library/BaseMemoryLib.h>
 #include <Library/MemoryAllocationLib.h>
 #include <Library/MallocLib.h>
 
+#define CPOOL_HEAD_SIGNATURE   SIGNATURE_32('C','p','h','d')
+
+typedef enum {
+  ALLOCTYPE_POOL,
+  ALLOCTYPE_ALIGNED_PAGES,
+} ALLOCATION_TYPE;
+
 typedef struct {
-  VOID                  *Memory;
-  UINTN                 Pages;
-  LIST_ENTRY            Link;
-} ALLOCATION_ENTRY;
+  UINT32                Signature;
+  UINTN                 Size;
+  UINTN                 Boundary;
+  ALLOCATION_TYPE       Type;
 
-STATIC LIST_ENTRY mAllocations = INITIALIZE_LIST_HEAD_VARIABLE(mAllocations);
+  UINT8 Data[0];
+} CPOOL_HEAD;
 
-VOID *malloc(UINTN size)
+VOID *memalign(UINTN Boundary, UINTN Size)
 {
-  return AllocatePool (size);
-}
+  VOID         *BaseMemory;
+  CPOOL_HEAD   *Head;
+  VOID         *RetVal;
+  UINTN         HeadSize;
+  UINTN         NodeSize;
+  ALLOCATION_TYPE Type;
 
-VOID *memalign(UINTN boundary, UINTN size)
-{
-  // allocate memory
-  UINTN Pages = EFI_SIZE_TO_PAGES(size);
-  VOID *Memory = AllocateAlignedPages(Pages, boundary);
-  if (Memory==NULL)
+  if (Size == 0) {
+    DEBUG((DEBUG_ERROR, "ERROR memalign: Zero Size\n"));
     return NULL;
-
-  // allocate node structure
-  ALLOCATION_ENTRY *NewNode = AllocatePool (sizeof (ALLOCATION_ENTRY));
-  if (NewNode == NULL) {
-    ASSERT (FALSE);
-    goto FreePages;
   }
 
-  NewNode->Memory     = Memory;
-  NewNode->Pages      = Pages;
+  if (Boundary == 8)
+    Type = ALLOCTYPE_POOL;
+  else
+    Type = ALLOCTYPE_ALIGNED_PAGES;
 
-  InsertTailList (&mAllocations, &NewNode->Link);
+  HeadSize = ALIGN_VALUE(sizeof(CPOOL_HEAD), Boundary);
+  NodeSize = HeadSize + Size;
 
-  return Memory;
+  DEBUG((DEBUG_POOL, "memalign(%d): NodeSz: %d", Size, NodeSize));
 
-FreePages:
-  FreeAlignedPages (Memory, Pages);
-  return NULL;
-}
+  if (Type == ALLOCTYPE_POOL)
+    BaseMemory = AllocatePool(NodeSize);
+  else if (Type == ALLOCTYPE_ALIGNED_PAGES)
+    BaseMemory = AllocateAlignedPages(EFI_SIZE_TO_PAGES(NodeSize), Boundary);
+  else
+    ASSERT(FALSE);
+  if (BaseMemory == NULL) {
+    RetVal  = NULL;
+    DEBUG((DEBUG_ERROR, "\nERROR memalign: alloc failed\n"));
+  }
+  else {
+    ASSERT(BaseMemory != NULL);
 
-VOID *calloc(UINTN count, UINTN size)
-{
-  return AllocateZeroPool (count * size);
-}
+    Head = BaseMemory + HeadSize - sizeof(CPOOL_HEAD);
 
-VOID free(VOID *Memory)
-{
-  LIST_ENTRY        *Link;
-  ALLOCATION_ENTRY  *Node;
+    // Fill out the pool header
+    Head->Signature = CPOOL_HEAD_SIGNATURE;
+    Head->Size      = Size;
+    Head->Boundary  = Boundary;
+    Head->Type      = Type;
 
-  if (Memory == NULL) {
-    return;
+    // Return a pointer to the data
+    RetVal          = (VOID*) Head->Data;
+    DEBUG((DEBUG_POOL, " Head: %p, Returns %p\n", Head, RetVal));
   }
 
-  // free aligned allocation
-  for (Link = mAllocations.ForwardLink; Link != &mAllocations; Link = Link->ForwardLink) {
-    Node = BASE_CR (Link, ALLOCATION_ENTRY, Link);
-    if (Node->Memory == Memory) {
-      FreeAlignedPages (Node->Memory, Node->Pages);
-      RemoveEntryList (&Node->Link);
-      FreePool (Node);
-      return;
+  return RetVal;
+}
+
+VOID *malloc(UINTN Size)
+{
+  return memalign(8, Size);
+}
+
+VOID *calloc(UINTN Count, UINTN Size)
+{
+  VOID *Ptr;
+
+  Ptr = malloc (Count * Size);
+  if (Ptr) {
+    SetMem (Ptr, Count * Size, 0);
+  }
+
+  return Ptr;
+}
+
+VOID free(VOID *Ptr)
+{
+  CPOOL_HEAD   *Head;
+  UINTN        HeadSize;
+  UINTN        NodeSize;
+  VOID         *BaseMemory;
+
+  if (Ptr != NULL) {
+    Head = BASE_CR(Ptr, CPOOL_HEAD, Data);
+    ASSERT(Head != NULL);
+    DEBUG((DEBUG_POOL, "free(%p): Head: %p\n", Ptr, Head));
+
+    if (Head->Signature == CPOOL_HEAD_SIGNATURE) {
+      HeadSize = ALIGN_VALUE(sizeof(CPOOL_HEAD), Head->Boundary);
+      NodeSize = HeadSize + Head->Size;
+      BaseMemory = Ptr - HeadSize;
+
+      if (Head->Type == ALLOCTYPE_POOL)
+        FreePool (BaseMemory);
+      else if (Head->Type == ALLOCTYPE_ALIGNED_PAGES)
+        FreeAlignedPages (BaseMemory, EFI_SIZE_TO_PAGES(NodeSize));
+      else
+        ASSERT(FALSE);
+    }
+    else {
+      DEBUG((DEBUG_ERROR, "ERROR free(0x%p): Signature is 0x%8X, expected 0x%8X\n",
+             Ptr, Head->Signature, CPOOL_HEAD_SIGNATURE));
     }
   }
+  DEBUG((DEBUG_POOL, "free Done\n"));
+}
 
-  // we don't know about this pointer, so assume this was a pool allocation
-  FreePool (Memory);
+VOID *realloc(VOID *Ptr, UINTN NewSize) {
+  VOID       *RetVal = NULL;
+  CPOOL_HEAD *Head    = NULL;
+  UINTN      OldSize = 0;
+  UINTN      NumCpy;
+
+  // Find out the size of the OLD memory region
+  if (Ptr != NULL) {
+    Head = BASE_CR (Ptr, CPOOL_HEAD, Data);
+    ASSERT (Head != NULL);
+    if (Head->Signature != CPOOL_HEAD_SIGNATURE) {
+      DEBUG((DEBUG_ERROR, "ERROR realloc(0x%p): Signature is 0x%8X, expected 0x%8X\n",
+             Ptr, Head->Signature, CPOOL_HEAD_SIGNATURE));
+      return NULL;
+    }
+    if (Head->Type != ALLOCTYPE_POOL) {
+      DEBUG((DEBUG_ERROR, "ERROR realloc(0x%p): Only malloc pointers are supported\n", Ptr));
+    }
+    OldSize = Head->Size;
+  }
+
+  // At this point, Ptr is either NULL or a valid pointer to an allocated space
+
+  if (NewSize == OldSize) {
+    RetVal = Ptr;
+  }
+  else if (NewSize > 0) {
+    RetVal = malloc(NewSize); // Get the NEW memory region
+    if (Ptr != NULL) {          // If there is an OLD region...
+      if (RetVal != NULL) {     // and the NEW region was successfully allocated
+        NumCpy = MIN(OldSize, NewSize);
+        CopyMem (RetVal, Ptr, NumCpy);  // Copy old data to the new region.
+        free (Ptr);                           // and reclaim the old region.
+      }
+    }
+  }
+  else {
+    free(Ptr);                           // Reclaim the old region.
+  }
+  DEBUG((DEBUG_POOL, "0x%p = realloc(%p, %d): Head: %p\n",
+         RetVal, Ptr, NewSize, Head));
+
+  return RetVal;
 }
