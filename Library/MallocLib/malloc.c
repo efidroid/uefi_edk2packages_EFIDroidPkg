40a5c2c359d0c3c9ff7313aaef74a0632857833d
M1cha <sigmaepsilon92@gmail.com>
2017-12-02 22:37:23 +0100

    malloc test

--- Library/MallocLib/malloc.c
+++ Library/MallocLib/malloc.c
@@ -2,20 +2,56 @@
 #include <Uefi.h>
 #include <Library/BaseLib.h>
 #include <Library/DebugLib.h>
+#include <Library/BaseMemoryLib.h>
 #include <Library/MemoryAllocationLib.h>
 #include <Library/MallocLib.h>
 
+typedef enum {
+  ALLOCTYPE_MALLOC,
+  ALLOCTYPE_MEMALIGN,
+} ALLOCATION_TYPE;
+
 typedef struct {
-  VOID                  *Memory;
-  UINTN                 Pages;
   LIST_ENTRY            Link;
+  ALLOCATION_TYPE       Type;
+  VOID                  *Memory;
+  UINTN                 Size;
+
+  union {
+    struct {
+      UINTN Boundary;
+      UINTN Pages;
+    } memalign;
+  } u;
 } ALLOCATION_ENTRY;
 
 STATIC LIST_ENTRY mAllocations = INITIALIZE_LIST_HEAD_VARIABLE(mAllocations);
 
 VOID *malloc(UINTN size)
 {
-  return AllocatePool (size);
+  // allocate memory
+  VOID *Memory = AllocatePool (size);
+  if (Memory==NULL)
+    return NULL;
+
+  // allocate node structure
+  ALLOCATION_ENTRY *NewNode = AllocatePool (sizeof (ALLOCATION_ENTRY));
+  if (NewNode == NULL) {
+    ASSERT (FALSE);
+    goto FreePages;
+  }
+
+  NewNode->Type       = ALLOCTYPE_MALLOC;
+  NewNode->Memory     = Memory;
+  NewNode->Size       = size;
+
+  InsertTailList (&mAllocations, &NewNode->Link);
+
+  return Memory;
+
+FreePages:
+  FreePool (Memory);
+  return NULL;
 }
 
 VOID *memalign(UINTN boundary, UINTN size)
@@ -33,8 +69,11 @@ VOID *memalign(UINTN boundary, UINTN size)
     goto FreePages;
   }
 
+  NewNode->Type       = ALLOCTYPE_MEMALIGN;
   NewNode->Memory     = Memory;
-  NewNode->Pages      = Pages;
+  NewNode->Size       = size;
+  NewNode->u.memalign.Boundary = boundary;
+  NewNode->u.memalign.Pages = Pages;
 
   InsertTailList (&mAllocations, &NewNode->Link);
 
@@ -63,13 +102,66 @@ VOID free(VOID *Memory)
   for (Link = mAllocations.ForwardLink; Link != &mAllocations; Link = Link->ForwardLink) {
     Node = BASE_CR (Link, ALLOCATION_ENTRY, Link);
     if (Node->Memory == Memory) {
-      FreeAlignedPages (Node->Memory, Node->Pages);
+      if (Node->Type == ALLOCTYPE_MALLOC) {
+        FreePool (Node->Memory);
+      }
+      else if (Node->Type == ALLOCTYPE_MEMALIGN) {
+        FreeAlignedPages (Node->Memory, Node->u.memalign.Pages);
+      }
+      else {
+        ASSERT(FALSE);
+      }
       RemoveEntryList (&Node->Link);
       FreePool (Node);
       return;
     }
+    else if (Memory >= Node->Memory && Memory < Node->Memory+Node->Size) {
+      DEBUG((DEBUG_ERROR, "tried to free from the middle\n"));
+    }
   }
 
-  // we don't know about this pointer, so assume this was a pool allocation
-  FreePool (Memory);
+  // we don't know about this pointer
+  DEBUG((DEBUG_ERROR, "cant free %p\n", Memory));
+  //ASSERT(FALSE);
+}
+
+VOID *realloc(VOID *Memory, UINTN size) {
+  LIST_ENTRY        *Link;
+  ALLOCATION_ENTRY  *Node;
+  VOID *NewMem = NULL;
+
+  if (Memory && !size) {
+    free(Memory);
+    return NULL;
+  }
+
+  if (!Memory)
+    return malloc(size);
+
+  for (Link = mAllocations.ForwardLink; Link != &mAllocations; Link = Link->ForwardLink) {
+    Node = BASE_CR (Link, ALLOCATION_ENTRY, Link);
+    if (Node->Memory == Memory) {
+      if (Node->Type == ALLOCTYPE_MALLOC) {
+        NewMem = malloc(size);
+      }
+      else if (Node->Type == ALLOCTYPE_MEMALIGN) {
+        NewMem = memalign(Node->u.memalign.Boundary, size);
+      }
+      else {
+        ASSERT(FALSE);
+      }
+
+      break;
+    }
+  }
+
+  // error
+  if (!NewMem)
+    return NULL;
+
+  ASSERT (Node);
+  CopyMem(NewMem, Memory, MIN(size, Node->Size));
+  free(Memory);
+
+  return NewMem;
 }
