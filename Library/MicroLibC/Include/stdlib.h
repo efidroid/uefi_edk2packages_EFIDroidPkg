12527245de844ffc12007341c2002cfe284ca06f
M1cha <sigmaepsilon92@gmail.com>
2017-12-17 14:16:00 +0100

    Library/MicroLibC: add bsearch

--- Library/MicroLibC/Include/stdlib.h
+++ Library/MicroLibC/Include/stdlib.h
@@ -1,6 +1,13 @@
 #ifndef _STDLIB_H
 #define _STDLIB_H
 
+#include <stdint.h>
+
 int atoi(const char *num);
+
+void *bsearch(
+    const void *key, const void *base, size_t nmemb, size_t size,
+    int (*compar)(const void *, const void *)
+);
 
 #endif
