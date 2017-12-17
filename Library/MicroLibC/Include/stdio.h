b35d75b027116888ad266a0e5f92df831b3f313c
M1cha <sigmaepsilon92@gmail.com>
2017-12-17 14:33:50 +0100

    Library/MicroLibC: add printf and vprintf

--- Library/MicroLibC/Include/stdio.h
+++ Library/MicroLibC/Include/stdio.h
@@ -7,4 +7,7 @@
 int vsnprintf(char *str, size_t size, const char *format, va_list ap);
 int snprintf(char *str, size_t size, const char *format, ...);
 
+int vprintf(const char *format, va_list ap);
+int printf(const char *format, ...);
+
 #endif
--- Library/MicroLibC/stdio.c
+++ Library/MicroLibC/stdio.c
@@ -1,6 +1,7 @@
 #include <Library/PrintLib.h>
 #include <Library/BaseLib.h>
 #include <Library/BaseMemoryLib.h>
+#include <Library/DebugLib.h>
 
 #include <stdio.h>
 
@@ -33,3 +34,21 @@ int snprintf(char *str, size_t size, const char *format, ...) {
     return err;
 }
 
+int vprintf(const char *format, va_list ap) {
+    CHAR8 buf[100];
+
+    vsnprintf(buf, ARRAY_SIZE(buf), format, ap);
+    DEBUG((DEBUG_ERROR, "%a", buf));
+    return 0;
+}
+
+int printf(const char *format, ...) {
+    int err;
+
+    VA_LIST ap;
+    VA_START(ap, format);
+    err = vprintf(format, ap);
+    VA_END(ap);
+
+    return err;
+}
