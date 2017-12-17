e26f0e3916909c4d13ee67d24d665d3f3b471527
M1cha <sigmaepsilon92@gmail.com>
2017-12-17 14:33:05 +0100

    Library/MicroLibC: add full atoi/ctypes from LK

--- Library/MicroLibC/Include/stdlib.h
+++ Library/MicroLibC/Include/stdlib.h
@@ -5,6 +5,11 @@
 #include <Library/MallocLib.h>
 
 int atoi(const char *num);
+unsigned int atoui(const char *num);
+long atol(const char *num);
+unsigned long atoul(const char *num);
+unsigned long long atoull(const char *num);
+unsigned long strtoul(const char *nptr, char **endptr, int base);
 
 void *bsearch(
     const void *key, const void *base, size_t nmemb, size_t size,
