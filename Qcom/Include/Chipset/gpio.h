5c61f6540b6c81be32c702506185e2c9b452d617
M1cha <sigmaepsilon92@gmail.com>
2017-12-30 09:51:00 +0100

    Qcom: move GpioTlmmDxe to GPLPkg and improve GpioTlmm protocol

--- Qcom/Include/Chipset/gpio.h
+++ Qcom/Include/Chipset/gpio.h
@@ -29,34 +29,6 @@
 #ifndef __CHIPSET_GPIO_H
 #define __CHIPSET_GPIO_H
 
-/* GPIO TLMM: Direction */
-#define GPIO_INPUT      0
-#define GPIO_OUTPUT     1
-
-/* GPIO TLMM: Pullup/Pulldown */
-#define GPIO_NO_PULL    0
-#define GPIO_PULL_DOWN  1
-#define GPIO_KEEPER     2
-#define GPIO_PULL_UP    3
-
-/* GPIO TLMM: Drive Strength */
-#define GPIO_2MA        0
-#define GPIO_4MA        1
-#define GPIO_6MA        2
-#define GPIO_8MA        3
-#define GPIO_10MA       4
-#define GPIO_12MA       5
-#define GPIO_14MA       6
-#define GPIO_16MA       7
-
-/* GPIO TLMM: Status */
-#define GPIO_ENABLE     0
-#define GPIO_DISABLE    1
-
-/* GPIO_IN_OUT register shifts. */
-#define GPIO_IN         BIT(0)
-#define GPIO_OUT        BIT(1)
-
 #define TLMM_PULL_MASK  0x3
 #define TLMM_HDRV_MASK  0x7
 
--- Qcom/Include/Protocol/QcomGpioTlmm.h
+++ Qcom/Include/Protocol/QcomGpioTlmm.h
@@ -8,16 +8,42 @@
 
 typedef struct _QCOM_GPIO_TLMM_PROTOCOL   QCOM_GPIO_TLMM_PROTOCOL;
 
-typedef void (EFIAPI *gpio_tlmm_config_t)(uint32_t gpio, uint8_t func, uint8_t dir, uint8_t pull, uint8_t drvstr, uint32_t enable);
-typedef void (EFIAPI *gpio_set_t)(uint32_t gpio, uint32_t dir);
-typedef uint32_t (EFIAPI *gpio_status_t)(uint32_t gpio);
+typedef enum {
+  GPIO_DIRECTION_OUT = 0,
+  GPIO_DIRECTION_IN = 1,
+} GPIO_DIRECTION;
+
+typedef enum {
+  GPIO_PULL_NONE = 0,
+  GPIO_PULL_DOWN = 1,
+  GPIO_PULL_KEEPER = 2,
+  GPIO_PULL_UP = 3,
+} GPIO_PULL;
+
+typedef EFI_STATUS (EFIAPI *msm_gpio_direction_input_t)(UINTN id);
+typedef EFI_STATUS (EFIAPI *msm_gpio_direction_output_t)(UINTN id, UINTN value);
+typedef GPIO_DIRECTION (EFIAPI *msm_gpio_get_direction_t)(UINTN id);
+typedef UINTN (EFIAPI *msm_gpio_get_t)(UINTN id);
+typedef VOID (EFIAPI *msm_gpio_set_t)(UINTN id, UINTN value);
+
+typedef EFI_STATUS (EFIAPI *msm_pinmux_set_function_t)(UINTN id, UINTN function);
+typedef EFI_STATUS (EFIAPI *msm_pinmux_set_drive_strength_t)(UINTN id, UINTN ma);
+typedef EFI_STATUS (EFIAPI *msm_pinmux_set_pull_t)(UINTN id, GPIO_PULL pull);
+
 typedef void (EFIAPI *tlmm_set_hdrive_ctrl_t)(struct tlmm_cfgs *, uint8_t);
 typedef void (EFIAPI *tlmm_set_pull_ctrl_t)(struct tlmm_cfgs *, uint8_t);
 
 struct _QCOM_GPIO_TLMM_PROTOCOL {
-  gpio_tlmm_config_t         gpio_tlmm_config;
-  gpio_set_t                 gpio_set;
-  gpio_status_t              gpio_status;
+  msm_gpio_direction_input_t          DirectionInput;
+  msm_gpio_direction_output_t         DirectionOutput;
+  msm_gpio_get_direction_t            GetDirection;
+  msm_gpio_get_t                      Get;
+  msm_gpio_set_t                      Set;
+
+  msm_pinmux_set_function_t           SetFunction;
+  msm_pinmux_set_drive_strength_t     SetDriveStrength;
+  msm_pinmux_set_pull_t               SetPull;
+
   tlmm_set_hdrive_ctrl_t     tlmm_set_hdrive_ctrl;
   tlmm_set_pull_ctrl_t       tlmm_set_pull_ctrl;
 };
