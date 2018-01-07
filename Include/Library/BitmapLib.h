b7e1f88b153c48d0207d1effb43ee0a73c3e7988
M1cha <sigmaepsilon92@gmail.com>
2018-01-07 09:08:45 +0100

    Include/Library/BitmapLib.h: add additional  macros as required by usb

--- Include/Library/BitmapLib.h
+++ Include/Library/BitmapLib.h
@@ -25,6 +25,9 @@
 
 #include <Library/AtomicLib.h>
 
+#define BIT_SHIFT(x, bit) (((x) >> (bit)) & 1)
+#define BITS(x, high, low) ((x) & (((1<<((high)+1))-1) & ~((1<<(low))-1)))
+#define BITS_SHIFT(x, high, low) (((x) >> (low)) & ((1<<((high)-(low)+1))-1))
 #define BIT_SET(x, bit) (((x) & (1UL << (bit))) ? 1 : 0)
 
 #define BITMAP_BITS_PER_WORD (sizeof(UINTN) * 8)
